7ef2085df1f694a439ed49bd73a90b0d1c198e43
24330 <24330@burnside.school.nz>
2026-03-10 12:41:44 +1300

    added lasers

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,6 +1,8 @@
 extends Area2D
 
 
+@onready var laser = preload("res://scenes/laser.tscn")
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -8,7 +10,10 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if Input.is_action_pressed("player_down"):
+	if Input.is_action_pressed("player_down") and position.y < 625:
 		position.y += 10
-	if Input.is_action_pressed("player_up"):
+	if Input.is_action_pressed("player_up") and position.y > 20:
 		position.y -= 10
+	if Input.is_action_just_pressed("shoot"):
+		#Make Laser
+		var laser = laser.instantiate()
